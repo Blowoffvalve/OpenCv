39bb50c2afcb70f6ce23dac1b3aadb74a5d2ab8f
Blowoffvalve <osunkoyaopeoluwa@gmail.com>
2019-01-11 22:45:49 -0600

    formatted the README.MD in the vanilla-opecv folder

--- vanilla_OpenCv/masking.py
+++ vanilla_OpenCv/masking.py
@@ -14,15 +14,42 @@ image = cv2.imread(args["image"])
 cv2.imshow("original", image)
 cv2.waitKey()
 
-#crop Jessica's Face
-mask = np.zeros(image.shape[:2], dtype="uint8")
-cv2.rectangle(mask, (80,10),(290,220), 255, -1)
-cv2.imshow("mask", mask)
+#Create a rectangular mask that captures Jessica's Face
+rectangle = np.zeros(image.shape[:2], dtype="uint8")
+cv2.rectangle(rectangle, (80,10),(290,220), 255, -1)
+cv2.imshow("Mask", rectangle)
 cv2.waitKey()
-cv2.imwrite("images\MaskForJessicaFace.jpg", mask)
 
-masked = cv2.bitwise_and(image, image, mask = mask)
-cv2.imshow("Masked", masked)
+#Mask her face using a rectangle
+rectangleMasked = cv2.bitwise_and(image, image, mask=rectangle)
+cv2.imshow("Masked", rectangleMasked)
 cv2.waitKey()
-cv2.imwrite("images\MaskOfJessicaFace.jpg", masked)
+
+#create a circular mask that captures Jessica's ace
+circle = np.zeros(image.shape[:2], dtype = "uint8")
+(cX, cY) = (int((290-80)/2)+80, int((220-10)/2)+10)
+cv2.circle(circle, (cX, cY), cY, 255, -1)
+circleMasked = cv2.bitwise_and(image, image, mask=circle)
+cv2.imshow("circleMasked", circleMasked)
+cv2.waitKey()
+
+#Write images to file
+r = 200/image.shape[1]
+dim = (200, int(image.shape[0] * r))
+
+maskshrunk = cv2.resize(rectangle, dim)
+cv2.imwrite("images\RectangleMaskForJessicaFace.jpg", rectangle)
+
+imageShrunk = cv2.resize(image, dim)
+cv2.imwrite("images\jpShrunk.png", imageShrunk)
+
+output = cv2.resize(rectangleMasked, dim)
+cv2.imwrite("images\JessicaFaceRectangleMasked.jpg", output)
+
+maskshrunk = cv2.resize(circle, dim)
+cv2.imwrite("images\CircleMaskForJessicaFace.jpg", circle)
+
+output = cv2.resize(circleMasked, dim)
+cv2.imwrite("images\JessicaFaceCircleMasked.jpg", output)
+
 cv2.destroyAllWindows()
